767adcad4740cf1b7d3d7a681ed1fcd982486e70
dmalembe <malembedidier24@gmail.com>
2021-05-24 21:18:56 +0200

    Initialisation du backend java spring boot

---
+++ freeparkings/src/main/java/com/entreprise/freeparkings/FreeparkingsApplication.java
@@ -0,0 +1,13 @@
+package com.entreprise.freeparkings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeparkingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeparkingsApplication.class, args);
+	}
+
+}
---
+++ freeparkings/src/test/java/com/entreprise/freeparkings/FreeparkingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entreprise.freeparkings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeparkingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
