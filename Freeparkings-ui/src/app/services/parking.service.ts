0bda5e31527338a2ac04541b747074c2b01e67f7
dmalembe <malembedidier24@gmail.com>
2021-07-09 09:49:47 +0200

    Last updates

--- Freeparkings-ui/src/app/services/parking.service.ts
+++ Freeparkings-ui/src/app/services/parking.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import { ParkingInfo } from '../parkingInfo';
 import { ParkingsComponent } from '../parkings/parkings.component';
 
@@ -13,11 +14,11 @@ export class ParkingService {
   constructor(private HttpClient: HttpClient) { }
 
   getParkings(): Observable<ParkingInfo[]> {
-    return this.HttpClient.get<ParkingInfo[]>("http://localhost:8080/parkings")
+    return this.HttpClient.get<ParkingInfo[]>(environment.apiUrl + "parkings")
   }
 
   getParking(id : number): Observable<ParkingInfo> {
-    return this.HttpClient.get<ParkingInfo[]>("http://localhost:8080/parkings").pipe(
+    return this.HttpClient.get<ParkingInfo[]>(environment.apiUrl + "parkings").pipe(
     map(parkings => parkings.find(parking => parking.id === id)) );
   }
 
